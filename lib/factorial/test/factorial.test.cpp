2c5b1cb579a188091a8557958828b4e7e5abba23
Samir Benmendil <me@rmz.io>
2017-05-21 11:10:10 +0100

    Add unit test target

---
+++ lib/factorial/test/factorial.test.cpp
@@ -0,0 +1,12 @@
+#include <catch.hpp>
+
+#include "factorial.hpp"
+
+SCENARIO("Factorials are computed")
+{
+    REQUIRE(factorial(0) == 1);
+    REQUIRE(factorial(1) == 1);
+    REQUIRE(factorial(2) == 2);
+    REQUIRE(factorial(3) == 6);
+    REQUIRE(factorial(10) == 3628800);
+}
